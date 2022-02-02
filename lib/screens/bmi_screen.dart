04efde36a04b331f42521afef4210783972f7480
Ahmet Burhan Simsek <ahmet-simsek@hotmail.com>
2022-02-02 12:35:06 +0300

    added screen navigation

---
+++ lib/screens/bmi_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:helloflutter/shared/menu_drawer.dart';
+
+class BmiScreen extends StatelessWidget {
+  const BmiScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('BMI Calculator')),
+      drawer: MenuDrawer(),
+      body: Center(child: FlutterLogo()),
+    );
+  }
+}
--- lib/shared/menu_drawer.dart
+++ lib/shared/menu_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../screens/bmi_screen.dart';
+import '../screens/intro_screen.dart';
 
 class MenuDrawer extends StatelessWidget {
   const MenuDrawer({Key? key}) : super(key: key);
@@ -29,9 +31,23 @@ class MenuDrawer extends StatelessWidget {
     ));
 
     menuTitles.forEach((String element) {
+      Widget screen = Container();
       menuItems.add(ListTile(
         title: Text(element, style: TextStyle(fontSize: 18)),
-        onTap: () {},
+        onTap: () {
+          switch (element) {
+            case 'Home':
+              screen = IntroScreen();
+              break;
+
+            case 'BMI Calculator':
+              screen = BmiScreen();
+              break;
+          }
+          Navigator.of(context).pop();
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => screen));
+        },
       ));
     });
 
